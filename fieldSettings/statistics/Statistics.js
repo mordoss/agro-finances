985ed8de3d9f90f0332454c2460343c70e7f4723
mordoss <milos8petrovic8@gmail.com>
2019-12-14 19:45:51 +0100

    refactore statistics

--- fieldSettings/statistics/Statistics.js
+++ fieldSettings/statistics/Statistics.js
@@ -5,15 +5,27 @@ import { Block, Text } from '../../components';
 const Statistics = ({ field }) => {
   const area = useSelector(state => state[field].area);
   const doneWorks = useSelector(state =>
-    Object.values(state[field].groundWorksState).filter(work => work.done && !work.paid)
+    Object.values(state[field].groundWorksState).filter(work => work.done)
   );
   const oilConsumptionDone =
-    (doneWorks.reduce((total, work) => total + Number(work.oilConsumption), 0) * area) / 100;
+    (doneWorks
+      .filter(work => !work.paid)
+      .reduce((total, work) => total + Number(work.oilConsumption), 0) *
+      area) /
+    100;
+
+  const paidDone =
+    (doneWorks
+      .filter(work => work.paid)
+      .reduce((total, work) => total + Number(work.paidPrice), 0) *
+      area) /
+    100;
 
   return (
     <Block>
       <Text>Litara nafte: {oilConsumptionDone}</Text>
       <Text>Nafta: {oilConsumptionDone * 150} </Text>
+      <Text>Uslužno: {paidDone}</Text>
     </Block>
   );
 };
