e216c9ada47a52edbf3d4ab78202f1f0b8fb902f
mordoss <milos8petrovic8@gmail.com>
2021-09-24 10:50:17 +0200

    resolve new asyncStorage

--- redux/store/index.js
+++ redux/store/index.js
@@ -1,6 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import { AsyncStorage } from 'react-native';
 import rootReducer from '../reducers';
 import wholeState from './wholeState';
 
