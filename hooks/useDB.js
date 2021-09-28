f1b9d5116812fdf755fea56093121238efb91dd5
mordoss <milos8petrovic8@gmail.com>
2021-09-28 21:34:33 +0200

    add fields loading from firebase

--- hooks/useDB.js
+++ hooks/useDB.js
@@ -1,15 +1,22 @@
 import Firebase from '../config/firebase';
 
 const useDB = () => {
+  const db = Firebase.database();
+
   const writeFieldsDB = (user, data) => {
-    Firebase.database()
-      .ref(`${user}/`)
-      .set({
-        ...data
-      });
+    db.ref(`${user}/`).set({
+      ...data
+    });
   };
 
-  return { writeFieldsDB };
+  const loadFieldsDB = (user, dispatch, actionCreator) => {
+    db.ref(`${user}/`).once('value', snapshot => {
+      const data = snapshot.val();
+      dispatch(actionCreator(data));
+    });
+  };
+
+  return { writeFieldsDB, loadFieldsDB };
 };
 
 export default useDB;
